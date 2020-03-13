c8c7f38f7bf3aa2eabcce430701c381fb1b477d3
joseph brown <jbrown562@west-mec.org>
2020-03-12 18:13:07 -0700

    finished case 1

--- case1/bw_review.js
+++ case1/bw_review.js
@@ -1,17 +1,14 @@
 "use strict";
-
 /*
    New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
    Tutorial 11
    Case Problem 1
-
-   Author: 
-   Date:   
+   Author: Caleb Brown
+   Date: 3-11-20  
    
    Filename: bw_review.js
-	
+   
    Functions List:
-
    init()
       Initializes the contents of the web page and sets up the
       event handlers.
@@ -23,41 +20,65 @@
    turnOffStars(e)
       Function that restores the stars to empty colors, removing
       the user rating for the book.
-
    updateCount()
       Updates the count of characters in the wordCountBox
       element.
-
    countCharacters(textStr)
       Returns the number of a non-whitespace characters
       within textStr
-
 */
-window.onload = init
-
+window.onload = init;
 function init(){
    var stars = document.querySelectorAll("span#stars img");
-
    for(var i = 0; i < stars.length; i++){
-      stars[i].style.backgroundColor;
-      stars[i].addEventListener("mouseenter","pointer");
+      stars[i].style.cursor = "pointer";
+      stars[i].addEventListener("mouseenter", lightStars);
    }
-   stars[i].addEventListener("count");
+   document.getElementById("commentField").addEventListener("keyup", updateCount);
 }
-function lightStars(){
-   
+function lightStars(e){
+   var starNumber = e.target.alt;
+   var stars = document.querySelectorAll("span#stars img");
+   for(var i = 0; i < stars.length; i++){
+      stars[i].src = "bw_star2.png";
+   }
+   for(var i = starNumber; i < 5; i++){
+      stars[i].src = "bw_star.png";
+   }
+   document.getElementById("rating").value = starNumber + " stars";
+   e.target.addEventListener("mouseleave", turnOffStars);
+   e.target.addEventListener("click",
+   function(){
+      e.target.removeEventListener("mouseleave", turnOffStars);
+   }
+   );
 }
-
-  
-
-
-  
-  
-  
+function turnOffStars(){
+   var stars = document.getElementsByClassName("span#stars img");
+   for(var i = 0; i < stars.length; i++){
+      stars[i].src = "bw_star.png";
+   }
+   document.getElementById("rating").value = "";
+}
+function updateCount(){
+   var commentText = document.getElementById("comment").value;
+   var charCount = countCharacters(commentText);
+   var wordCountBox = document.getElementById("wordCount");
+   wordCountBox.value = charCount + "/1000";
+   if(charCount > 1000){
+      wordCountBox.style.backgroundColor = "red";
+      wordCountBox.style.color = "white";
+   }else{
+      wordCountBox.style.backgroundColor = "white";
+      wordCountBox.style.color = "black";
+   }
+}
 /*=================================================================*/
-
 function countCharacters(textStr) {
    var commentregx = /\s/g;
    var chars = textStr.replace(commentregx, "");
    return chars.length;
-}   
+}
+
+
+
